cbe232e702931290f7d093bda812040e8c6d17fb
Nikita Kurguzov <vrolok15@yandex.ru>
2025-03-03 15:45:07 +0300

    Added loaded image to canvas

--- src/components/PixelCanvas.vue
+++ src/components/PixelCanvas.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="canvas-wrapper">
     <div class="canvas-container">
-      <canvas id="pixel-canvas"></canvas>
-      <div class="center-button">
+      <canvas ref="canvas" id="pixel-canvas"></canvas>
+      <div class="center-button" v-if="!hasImage">
         <PixelButton @click="triggerFileInput">Load</PixelButton>
         <input 
           type="file" 
@@ -15,7 +15,7 @@
     </div>
     <div class="buttons-container">
       <PixelButton>Save</PixelButton>
-      <PixelButton>Clear</PixelButton>
+      <PixelButton @click="clearCanvas">Clear</PixelButton>
     </div>
   </div>
 </template>
@@ -28,16 +28,79 @@ export default {
   components: {
     PixelButton
   },
+  data() {
+    return {
+      hasImage: false,
+      ctx: null,
+      canvasSize: 512 // Default canvas size
+    }
+  },
+  mounted() {
+    this.initCanvas()
+  },
   methods: {
+    initCanvas() {
+      const canvas = this.$refs.canvas
+      // Set actual canvas dimensions (not CSS dimensions)
+      canvas.width = this.canvasSize
+      canvas.height = this.canvasSize
+      this.ctx = canvas.getContext('2d')
+      this.ctx.imageSmoothingEnabled = false
+    },
     triggerFileInput() {
       this.$refs.fileInput.click()
     },
     handleFileSelect(event) {
       const file = event.target.files[0]
       if (file) {
-        console.log('Selected file:', file.name)
-        // We'll handle the file loading in the next step
+        const reader = new FileReader()
+        reader.onload = (e) => {
+          const img = new Image()
+          img.onload = () => {
+            this.drawImage(img)
+          }
+          img.src = e.target.result
+        }
+        reader.readAsDataURL(file)
       }
+    },
+    drawImage(img) {
+      const canvas = this.$refs.canvas
+      
+      // Calculate scale to fit the image while maintaining aspect ratio
+      let scale = 1
+      if (img.width > img.height) {
+        // If image is wider than tall, scale based on width
+        scale = Math.floor(this.canvasSize / img.width)
+      } else {
+        // If image is taller than wide, scale based on height
+        scale = Math.floor(this.canvasSize / img.height)
+      }
+
+      // Calculate centered position
+      const scaledWidth = img.width * scale
+      const scaledHeight = img.height * scale
+      const x = Math.floor((canvas.width - scaledWidth) / 2)
+      const y = Math.floor((canvas.height - scaledHeight) / 2)
+
+      // Clear canvas
+      this.ctx.clearRect(0, 0, canvas.width, canvas.height)
+      
+      // Draw image with integer scaling
+      this.ctx.drawImage(
+        img,
+        x,
+        y,
+        scaledWidth,
+        scaledHeight
+      )
+
+      this.hasImage = true
+    },
+    clearCanvas() {
+      const canvas = this.$refs.canvas
+      this.ctx.clearRect(0, 0, canvas.width, canvas.height)
+      this.hasImage = false
     }
   }
 }
@@ -56,6 +119,7 @@ export default {
   position: relative;
   width: 80vmin;
   height: 80vmin;
+  background-color: var(--color-element);
 }
 
 #pixel-canvas {
@@ -63,6 +127,9 @@ export default {
   width: 100%;
   height: 100%;
   display: block;
+  image-rendering: pixelated;
+  image-rendering: crisp-edges;
+  background-color: var(--color-element);
 }
 
 .center-button {
